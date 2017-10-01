0acc3e65fc46c024014dc7d2836aeadce8f3eefc
chuk-chuk <yulia.v.litvin@gmail.com>
2017-10-01 13:33:08 +0100

    it can store different options depending on the user selection

--- lib/generator.rb
+++ lib/generator.rb
@@ -9,4 +9,13 @@ class Generator
     @special = special
     @options = []
   end
+
+  def add_to_options
+    options.push(("A".."Z").to_a) if uppercase
+    options.push(("a".."z").to_a) if lowercase
+    options.push((1..9).to_a) if number
+    options.push("!","$","%","&","*","@","^") if special
+    options
+  end
+
 end
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -3,26 +3,41 @@ require 'generator'
 describe Generator do
   subject(:generator) { described_class.new(length, uppercase, lowercase, number, special) }
   let(:length) { 5 }
-  let(:uppercase) { true }
-  let(:lowercase) { true }
+  let(:uppercase) { false }
+  let(:lowercase) { false }
   let(:number) { true }
-  let(:special) { false }
+  let(:special) { true }
 
+  describe "initialized" do
+    it "initialized with length" do
+      expect(subject.length).to eq length
+    end
 
-  it "initialized with length" do
-    expect(subject.length).to eq length
+    it "initialized with uppercase" do
+      expect(subject.uppercase).to eq uppercase
+    end
+
+    it "initialized with special" do
+      expect(subject.special).to eq special
+    end
+
+    it "starts with empty options array" do
+      expect(subject.options).to eq []
+    end
   end
 
-  it "initialized with uppercase" do
-    expect(subject.uppercase).to eq uppercase
+  it "can store options in the options array" do
+    generator.add_to_options
+    expect(generator.options).not_to be_empty
   end
 
-  it "initialized with special" do
-    expect(subject.special).to eq special
+  it "can store numbers in the options array" do
+    generator.add_to_options
+    expect(generator.options).to include [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  it "starts with empty options array" do
-    expect(subject.options).to eq []
+  it "can store special characters in the options array" do
+    generator.add_to_options
+    expect(generator.options).to include "!"
   end
-
 end
